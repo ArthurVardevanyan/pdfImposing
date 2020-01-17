0a8e197a768d4f42105f13735da1f6fcc088320b
Arthur <21159051+ArthurVardevanyan@users.noreply.github.com>
2020-01-17 16:14:51 -0500

    180 Degree Pages

--- nup.py
+++ nup.py
@@ -5,14 +5,26 @@ import PyPDF2
 
 # Starting Point https://github.com/mstamy2/PyPDF2/blob/master/Scripts/2-up.py
 
+
 def nup(inFile):
- #Still only does 2up from the adapted script.
+ # Still only does 2up from the adapted script.
     doc = PyPDF2.PdfFileReader(inFile, "rb")
     output = PyPDF2.PdfFileWriter()
     for iter in range(0, doc.getNumPages()-1, 2):
         lhs = doc.getPage(iter)
         rhs = doc.getPage(iter+1)
-        lhs.mergeTranslatedPage(rhs, lhs.mediaBox.getUpperRight_x(), 0, True)
+        rotationL = lhs.get('/Rotate')
+        rotationR = rhs.get('/Rotate')
+        if(rotationL):
+            lhs.mergeRotatedTranslatedPage(
+                rhs, 180, 0,  int(lhs.mediaBox.getUpperRight_y()/2), True)
+            
+        elif(rotationR):
+            lhs.mergeRotatedTranslatedPage(
+                rhs, 180, lhs.mediaBox.getUpperRight_x(),  int(lhs.mediaBox.getUpperRight_y()/2), True)
+        else:
+            lhs.mergeTranslatedPage(
+                rhs, lhs.mediaBox.getUpperRight_x(), 0, True)
         output.addPage(lhs)
 
     outputStream = open(inFile[:-4] + "_nup.pdf", "wb")
@@ -21,7 +33,7 @@ def nup(inFile):
 
 def main():
 
-    nup("sample/8page_stackShuffled.pdf")
+    nup("sample/8page_normalShuffled.pdf")
 
 
 if __name__ == "__main__":
