71d5cab49331894f025de8cf447c88d9dc6df5f5
Arthur <arthur@arthurvardevanyan.com>
2020-01-17 22:11:38 -0500

    booklets

--- pdfImposer.py
+++ pdfImposer.py
@@ -35,11 +35,38 @@ def ledgerDuplexTwoUpSpinCut(inFile):
     if os.path.exists(filePath):
         os.remove(filePath)
 
+def booklet(inFile):
+    doc = PyPDF2.PdfFileReader(open(inFile, "rb"))
+    print("Margins")
+    output = margin.margin(doc)
+    print("Booklet Shuffle 1")
+    output = shuffle.bookletShuffle(output)
+    print("Booklet Shuffle 2")
+    temp = "temp.pdf"
+    outputStream = open(temp, "wb")
+    output.write(outputStream)
+    outputStream.close()
+    inFileR = open(temp, "rb")
+    docR = PyPDF2.PdfFileReader(inFileR)
+    output = shuffle.normalShuffle(output, docR, "1 2 4 3 ")
+    print("Nup")
+    output = nup.nup(output)
+    outputStream = open(inFile[:-4] + "_booklet.pdf", "wb")
+    output.write(outputStream)
+
+    inFileR.close()
+    outputStream.close()
+
+    filePath = "temp.pdf"
+    if os.path.exists(filePath):
+        os.remove(filePath)
 
 def main():
     inFile = sys.argv[1]
     #inFile = "sample/blackPage.pdf"
-    ledgerDuplexTwoUpSpinCut(inFile)
+    #ledgerDuplexTwoUpSpinCut(inFile)
+
+    booklet(inFile)
 
 
 if __name__ == "__main__":
