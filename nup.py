356dd0ba820c6a4f04ae34e971c25d66a233772d
Arthur <arthur@arthurvardevanyan.com>
2020-01-17 19:02:27 -0500

    modulatrity adjustments

--- nup.py
+++ nup.py
@@ -6,11 +6,11 @@ import PyPDF2
 # Starting Point https://github.com/mstamy2/PyPDF2/blob/master/Scripts/2-up.py
 
 
-def nup(inFile):
+def nup(doc):
  # Still only does 2up from the adapted script.
-    doc = PyPDF2.PdfFileReader(inFile, "rb")
     output = PyPDF2.PdfFileWriter()
     for iter in range(0, doc.getNumPages()-1, 2):
+        # print(iter)
         lhs = doc.getPage(iter)
         rhs = doc.getPage(iter+1)
         rotationL = lhs.get('/Rotate')
@@ -18,7 +18,7 @@ def nup(inFile):
         if(rotationL):
             lhs.mergeRotatedTranslatedPage(
                 rhs, 180, 0,  int(lhs.mediaBox.getUpperRight_y()/2), True)
-            
+
         elif(rotationR):
             lhs.mergeRotatedTranslatedPage(
                 rhs, 180, lhs.mediaBox.getUpperRight_x(),  int(lhs.mediaBox.getUpperRight_y()/2), True)
@@ -27,13 +27,15 @@ def nup(inFile):
                 rhs, lhs.mediaBox.getUpperRight_x(), 0, True)
         output.addPage(lhs)
 
-    outputStream = open(inFile[:-4] + "_nup.pdf", "wb")
-    output.write(outputStream)
+    return output
 
 
 def main():
-
-    nup("sample/8page_normalShuffled.pdf")
+    inFile = "sample/blackPage_scaled_normalShuffled.pdf"
+    doc = PyPDF2.PdfFileReader(inFile, "rb")
+    output = nup(doc)
+    outputStream = open(inFile[:-4] + "_nup.pdf", "wb")
+    output.write(outputStream)
 
 
 if __name__ == "__main__":
--- shuffle.py
+++ shuffle.py
@@ -4,47 +4,45 @@ __version__ = "v20200117"
 import PyPDF2
 
 
-def normalShuffle(inFile, str):
-
-    doc = PyPDF2.PdfFileReader(open(inFile, "rb"))
-    docR = PyPDF2.PdfFileReader(open(inFile, "rb"))
+def normalShuffle(doc, docR, str):
 
     output = PyPDF2.PdfFileWriter()
     #RULES = [int(s) for s in str.split() if s.isdigit()]
     RULES = list(str)[::2]
-    RULE_MOD = list(str)[1::2] 
+    RULE_MOD = list(str)[1::2]
 
     pages = []
     for iter in range(0, int(doc.getNumPages()), len(set(RULES))):
 
         for i in range(0, len(RULES)):
             if (RULE_MOD[i] == "*"):
-                pages.append(docR.getPage(iter+int(RULES[i])-1).rotateClockwise(180))
+                pages.append(docR.getPage(
+                    iter+int(RULES[i])-1).rotateClockwise(180))
             else:
                 pages.append(doc.getPage(iter+int(RULES[i])-1))
 
     for page in pages:
         output.addPage(page)
 
-    outputStream = open(inFile[:-4] + "_normalShuffled.pdf", "wb")
-    output.write(outputStream)
+    return output
 
 
-def stackShuffle(inFile, str):
+def stackShuffle(doc, str):
 
-    doc = PyPDF2.PdfFileReader(open(inFile, "rb"))
     output = PyPDF2.PdfFileWriter()
     RULES = [int(s) for s in str.split() if s.isdigit()]
     pages = []
     temp = PyPDF2.PdfFileWriter()
     for iter in range(0, (doc.getNumPages())):
+        # print(iter)
         temp.addPage(doc.getPage(iter))
     doc = temp
     while((doc.getNumPages() % len(RULES))):
         doc.addPage(PyPDF2.pdf.PageObject.createBlankPage(doc.getPage(0), doc.getPage(
             0).mediaBox.getWidth(), doc.getPage(0).mediaBox.getHeight()))
-    print(doc.getNumPages())
+    # print(doc.getNumPages())
     for iter in range(0, int(doc.getNumPages()/len(RULES))):
+        # print(iter)
         for i in range(0, len(RULES)):
             pages.append(doc.getPage(
                 iter+(int(RULES[i])-1)*int(doc.getNumPages()/len(RULES))))
@@ -52,14 +50,19 @@ def stackShuffle(inFile, str):
     for page in pages:
         output.addPage(page)
 
-    outputStream = open(inFile[:-4] + "_stackShuffled.pdf", "wb")
-    output.write(outputStream)
+    return output
 
 
 def main():
 
-    #Space For No Modifier, * For 180 Flip
-    normalShuffle("sample/8page.pdf", "1 1*2*2 ")
+    inFile = "sample/blackPage_scaled.pdf"
+    doc = PyPDF2.PdfFileReader(open(inFile, "rb"))
+    docR = PyPDF2.PdfFileReader(open(inFile, "rb"))
+
+    # Space For No Modifier, * For 180 Flip
+    output = normalShuffle(doc, docR, "1 1*")
+    outputStream = open(inFile[:-4] + "_stackShuffled.pdf", "wb")
+    output.write(outputStream)
 
 
 if __name__ == "__main__":
