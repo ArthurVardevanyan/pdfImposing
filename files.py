9fceede64f26a8262372d5a5d5780cadafb064e3
Arthur <arthur@arthurvardevanyan.com>
2020-01-20 17:15:42 -0500

    File Merge Simplex

---
+++ files.py
@@ -0,0 +1,15 @@
+# files.py
+__version__ = "v20200120"
+
+import os
+import glob
+
+
+def file_list(folder):
+    # Grabs the PDF's in the requested order
+    fileList = sorted(glob.glob("".join([folder, "/*.pdf"]))
+                      )  # Gathers all the Files
+    # Strips the file path data to leave just the filename
+    Stripped_List = [os.path.basename(x) for x in fileList]
+    return Stripped_List  # Returns the Stripped List to Main Function
+
