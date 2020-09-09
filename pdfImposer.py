53e4913cd2ec7e38b06630e65354d2396c11e84e
Arthur <21159051+ArthurVardevanyan@users.noreply.github.com>
2020-09-09 16:33:49 -0400

    Booklet GUI

--- pdfImposer.py
+++ pdfImposer.py
@@ -4,15 +4,37 @@ __version__ = "v20200206"
 
 import tkinter as tk
 from tkinter import filedialog
+import scripts
 
-def main():
+class pdfImposing:
+    def __init__(self, master):
+        self.master = master
+        master.title("pdfImposing")
 
-    
-    window = tk.Tk()
-    greeting = tk.Label(text="Hello, Tkinter")
-    greeting.pack()
-    window.mainloop()
+        self.label = tk.Label(master, text="PDF Imposing")
+        self.label.pack()
+
+        self.upload = tk.Button(root, text='Open', command=self.UploadAction)
+        self.upload.pack()
+        self.files = None
+
+        self.bookletLabel = tk.Button(root, text='Create Booklet', command=self.booklet)
+        self.bookletLabel.pack()
 
 
-if __name__ == "__main__":
-    main()
+    def UploadAction(self):
+        # https://stackoverflow.com/a/50135930
+        filenames = filedialog.askopenfilenames()
+        print('Selected:', filenames)
+        self.files = filenames
+        self.label = tk.Label(text=filenames)
+        self.label.pack()
+
+    def booklet(self):
+        scripts.booklet(self.files[0])
+        
+
+
+root = tk.Tk()
+my_gui = pdfImposing(root)
+root.mainloop()
