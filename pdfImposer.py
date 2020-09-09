e295956ec24d6e50187f5be5bc2a8e621fd337a9
Arthur <arthur@arthurvardevanyan.com>
2020-09-09 16:11:36 -0400

    GUI

--- pdfImposer.py
+++ pdfImposer.py
@@ -1,125 +1,17 @@
 # pdfImposer.py
 __version__ = "v20200206"
 
-import PyPDF2
-import os
-import sys
-import time
-import margin
-import shuffle
-import nup
 
-
-def ledgerDuplexTwoUpSpinCut(inFile, outFile=None):
-    if outFile == None:
-        outFile = inFile
-        append = "_modified.pdf"
-    else:
-        append = ".pdf"
-
-    doc = PyPDF2.PdfFileReader(open(inFile, "rb"))
-    print("Margins")
-    output = margin.margin(doc)
-    temp = "temp.pdf"
-    outputStream = open(temp, "wb")
-    output.write(outputStream)
-    outputStream.close()
-    print("Shuffle")
-    inFileR = open(temp, "rb")
-    docR = PyPDF2.PdfFileReader(inFileR)
-    output = shuffle.normalShuffle(output, docR, "1*1 2 2*")
-    print("Nup")
-    output = nup.nup(output)
-    outputStream = open(outFile[:-4] + append, "wb")
-    output.write(outputStream)
-
-    inFileR.close()
-    outputStream.close()
-
-    filePath = "temp.pdf"
-    if os.path.exists(filePath):
-        os.remove(filePath)
-
-
-def ledgerSimplexTwoUp(inFile, outFile=None):
-    if outFile == None:
-        outFile = inFile
-        append = "_modified.pdf"
-    else:
-        append = ".pdf"
-
-    doc = PyPDF2.PdfFileReader(open(inFile, "rb"))
-    print("Margins")
-    output = margin.margin(doc)
-    print("Shuffle")
-    output = shuffle.normalShuffle(output, output, "1 1 ")
-    print("Nup")
-    output = nup.nup(output)
-    outputStream = open(outFile[:-4] + append, "wb")
-    output.write(outputStream)
-
-    outputStream.close()
-
-
-def booklet(inFile):
-    doc = PyPDF2.PdfFileReader(open(inFile, "rb"))
-    print("Margins")
-    output = margin.margin(doc)
-    print("Booklet Shuffle 1")
-    output = shuffle.bookletShuffle(output)
-    print("Booklet Shuffle 2")
-    temp = "temp.pdf"
-    outputStream = open(temp, "wb")
-    output.write(outputStream)
-    outputStream.close()
-    inFileR = open(temp, "rb")
-    docR = PyPDF2.PdfFileReader(inFileR)
-    output = shuffle.normalShuffle(output, docR, "1 2 4 3 ")
-    print("Nup")
-    output = nup.nup(output)
-    outputStream = open(inFile[:-4] + "_booklet.pdf", "wb")
-    output.write(outputStream)
-
-    inFileR.close()
-    outputStream.close()
-
-    filePath = "temp.pdf"
-    if os.path.exists(filePath):
-        os.remove(filePath)
-
-
-def SimplexStackCut(inFile):
-
-    print("StackShuffle")
-    doc = PyPDF2.PdfFileReader(open(inFile, "rb"))
-    output = shuffle.stackShuffle(doc, "1 2")
-    print("Nup")
-    output = nup.nup(output)
-    outputStream = open(inFile[:-4] + "_simplexStackShuffledNup.pdf", "wb")
-    output.write(outputStream)
-
-
-def DuplexStackCut(inFile):
-
-    doc = PyPDF2.PdfFileReader(open(inFile, "rb"))
-    docR = PyPDF2.PdfFileReader(open(inFile, "rb"))
-    print("Duplex Shuffle")
-    output = shuffle.normalShuffle(doc, docR, "1 2 4 3 ")
-    print("StackShuffle")
-    output = shuffle.stackShuffle(output, "1 2")
-    print("Nup")
-    output = nup.nup(output)
-    outputStream = open(inFile[:-4] + "_DuplexStackShuffledNup.pdf", "wb")
-    output.write(outputStream)
-
+import tkinter as tk
+from tkinter import filedialog
 
 def main():
-    inFile = sys.argv[1]
 
-    SimplexStackCut(inFile)
-    # inFile = "sample/blackPage.pdf"
-    # ledgerDuplexTwoUpSpinCut(inFile)
-    # booklet(inFile)
+    
+    window = tk.Tk()
+    greeting = tk.Label(text="Hello, Tkinter")
+    greeting.pack()
+    window.mainloop()
 
 
 if __name__ == "__main__":
---
+++ scripts.py
@@ -0,0 +1,141 @@
+
+__version__ = "v20200206"
+import os
+import sys
+import PyPDF2
+import time
+import margin
+import shuffle
+import nup
+
+def ledgerDuplexTwoUpSpinCut(inFile, outFile=None):
+    if outFile == None:
+        outFile = inFile
+        append = "_modified.pdf"
+    else:
+        append = ".pdf"
+
+    doc = PyPDF2.PdfFileReader(open(inFile, "rb"))
+    print("Margins")
+    output = margin.margin(doc)
+    temp = "temp.pdf"
+    outputStream = open(temp, "wb")
+    output.write(outputStream)
+    outputStream.close()
+    print("Shuffle")
+    inFileR = open(temp, "rb")
+    docR = PyPDF2.PdfFileReader(inFileR)
+    output = shuffle.normalShuffle(output, docR, "1*1 2 2*")
+    print("Nup")
+    output = nup.nup(output)
+    outputStream = open(outFile[:-4] + append, "wb")
+    output.write(outputStream)
+
+    inFileR.close()
+    outputStream.close()
+
+    filePath = "temp.pdf"
+    if os.path.exists(filePath):
+        os.remove(filePath)
+
+
+def ledgerSimplexTwoUp(inFile, outFile=None):
+    if outFile == None:
+        outFile = inFile
+        append = "_modified.pdf"
+    else:
+        append = ".pdf"
+
+    doc = PyPDF2.PdfFileReader(open(inFile, "rb"))
+    print("Margins")
+    output = margin.margin(doc)
+    print("Shuffle")
+    output = shuffle.normalShuffle(output, output, "1 1 ")
+    print("Nup")
+    output = nup.nup(output)
+    outputStream = open(outFile[:-4] + append, "wb")
+    output.write(outputStream)
+
+    outputStream.close()
+
+
+def ledgerSimplexTwoUpSpinCut(inFile, outFile=None):
+    if outFile == None:
+        outFile = inFile
+        append = "_modified.pdf"
+    else:
+        append = ".pdf"
+
+    doc = PyPDF2.PdfFileReader(open(inFile, "rb"))
+    print("Margins")
+    output = margin.margin(doc)
+    print("Shuffle")
+    output = shuffle.normalShuffle(output, output, "1*1 ")
+    print("Nup")
+    output = nup.nup(output)
+    outputStream = open(outFile[:-4] + append, "wb")
+    output.write(outputStream)
+
+    outputStream.close()
+
+
+def booklet(inFile):
+    doc = PyPDF2.PdfFileReader(open(inFile, "rb"))
+    print("Margins")
+    output = margin.margin(doc)
+    print("Booklet Shuffle 1")
+    output = shuffle.bookletShuffle(output)
+    print("Booklet Shuffle 2")
+    temp = "temp.pdf"
+    outputStream = open(temp, "wb")
+    output.write(outputStream)
+    outputStream.close()
+    inFileR = open(temp, "rb")
+    docR = PyPDF2.PdfFileReader(inFileR)
+    output = shuffle.normalShuffle(output, docR, "1 2 4 3 ")
+    print("Nup")
+    output = nup.nup(output)
+    outputStream = open(inFile[:-4] + "_booklet.pdf", "wb")
+    output.write(outputStream)
+
+    inFileR.close()
+    outputStream.close()
+
+    filePath = "temp.pdf"
+    if os.path.exists(filePath):
+        os.remove(filePath)
+
+
+def SimplexStackCut(inFile):
+
+    print("StackShuffle")
+    doc = PyPDF2.PdfFileReader(open(inFile, "rb"))
+    output = shuffle.stackShuffle(doc, "1 2")
+    print("Nup")
+    output = nup.nup(output)
+    outputStream = open(inFile[:-4] + "_simplexStackShuffledNup.pdf", "wb")
+    output.write(outputStream)
+
+
+def DuplexStackCut(inFile):
+
+    doc = PyPDF2.PdfFileReader(open(inFile, "rb"))
+    docR = PyPDF2.PdfFileReader(open(inFile, "rb"))
+    print("Duplex Shuffle")
+    output = shuffle.normalShuffle(doc, docR, "1 2 4 3 ")
+    print("StackShuffle")
+    output = shuffle.stackShuffle(output, "1 2")
+    print("Nup")
+    output = nup.nup(output)
+    outputStream = open(inFile[:-4] + "_DuplexStackShuffledNup.pdf", "wb")
+    output.write(outputStream)
+
+
+
+
+    #inFile = sys.argv[1]
+
+    #SimplexStackCut(inFile)
+    # inFile = "sample/blackPage.pdf"
+    # ledgerDuplexTwoUpSpinCut(inFile)
+    # booklet(inFile)
