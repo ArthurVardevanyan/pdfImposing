4c096014f6550db2736fcadc7ce9df01c46e3bfd
Arthur <arthur@arthurvardevanyan.com>
2020-09-12 14:26:29 -0400

    Scalable Booklet Margin

--- pdfImposer.py
+++ pdfImposer.py
@@ -14,52 +14,56 @@ class pdfImposing:
         root.title("pdfImposing: " + __version__)
 
         tk.Label(root, text="PDF Imposing: " +
-                 __version__).grid(row=0, column=1, pady=2)
+                 __version__).grid(row=0, column=1, padx=5, pady=2)
 
         tk.Button(root, text='Open Files(s)',
-                  command=self.UploadAction).grid(row=1, column=1, pady=2)
+                  command=self.UploadAction).grid(row=1, column=1, padx=5, pady=2)
 
         self.FILES = []
         self.files = None
         self.fileLabel = tk.Label()
 
         self.duplex = tk.StringVar(value="False")
-        tk.Label(root, text="File Name:").grid(row=3, column=0, pady=2)
+        tk.Label(root, text="File Name:").grid(row=3, column=0, padx=5, pady=2)
         self.newName = tk.Entry(root)
-        self.newName.grid(row=4, column=0, pady=0)
+        self.newName.grid(row=4, column=0, padx=5, pady=0)
         tk.Radiobutton(root, text='Simplex',
-                       variable=self.duplex, value="False").grid(row=5, column=0, pady=0)
+                       variable=self.duplex, value="False").grid(row=5, column=0, padx=5, pady=0)
 
         tk.Radiobutton(root, text='Duplex',
-                       variable=self.duplex, value="True").grid(row=6, column=0, pady=0)
+                       variable=self.duplex, value="True").grid(row=6, column=0, padx=5, pady=0)
 
         tk.Button(root, text='FileMerge',
-                  command=self.fileMerge).grid(row=7, column=0, pady=1)
+                  command=self.fileMerge).grid(row=7, column=0, padx=5, pady=1)
 
-        tk.Label(root, text="CSV:").grid(row=3, column=1, pady=0)
+        tk.Label(root, text="CSV:").grid(row=3, column=2, padx=5, pady=0)
         self.pageNumbers = tk.Entry()
-        self.pageNumbers.grid(row=4, column=1, pady=1)
+        self.pageNumbers.grid(row=4, column=2, padx=5, pady=1)
 
         tk.Button(root, text='AddBlankPage(s)', command=self.addSave).grid(
-            row=5, column=1, pady=0)
+            row=5, column=2, padx=5, pady=0)
         tk.Button(root, text='removePage(s)', command=self.removeSave).grid(
-            row=6, column=1, pady=0)
-
-        tk.Label(root, text="Other:").grid(row=3, column=2, pady=2)
+            row=6, column=2, padx=5, pady=0)
+        tk.Label(root, text="Margin Decimal Percent:").grid(
+            row=3, column=1, padx=5, pady=0)
+        self.margin = tk.Entry()
+        self.margin.insert(0, ".96")
+        self.margin.grid(row=4, column=1, padx=5, pady=0)
         tk.Button(root, text='Create Booklet', command=self.booklet).grid(
-            row=4, column=2, pady=(0, 15))
+            row=5, column=1, padx=5, pady=3)
+        
+        
+        tk.Button(
+            root, text='SimplexStackCut', command=self.SimplexStackCut).grid(row=7, column=1, padx=5, pady=0)
 
         tk.Button(
-            root, text='SimplexStackCut', command=self.SimplexStackCut).grid(row=5, column=2, pady=0)
-
-        tk.Button(
-            root, text='DuplexStackCut', command=self.DuplexStackCut).grid(row=6, column=2, pady=(0, 15))
+            root, text='DuplexStackCut', command=self.DuplexStackCut).grid(row=8, column=1, padx=5, pady=(0, 15))
         tk.Button(root, text='ledgerSimplexTwoUp',
-                  command=self.ledgerSimplexTwoUp).grid(row=7, column=2, pady=0)
+                  command=self.ledgerSimplexTwoUp).grid(row=9, column=1, padx=5, pady=0)
         tk.Button(root, text='ledgerDuplexTwoUpSpinCut',
-                  command=self.ledgerDuplexTwoUpSpinCut).grid(row=8, column=2, pady=0)
+                  command=self.ledgerDuplexTwoUpSpinCut).grid(row=10, column=1, padx=5, pady=0)
         tk.Button(root, text='ledgerSimplexTwoUpSpinCut',
-                  command=self.ledgerSimplexTwoUpSpinCut).grid(row=9, column=2, pady=0)
+                  command=self.ledgerSimplexTwoUpSpinCut).grid(row=11, column=1, padx=5, pady=0)
 
     def UploadAction(self):
         # https://stackoverflow.com/a/50135930
@@ -87,7 +91,7 @@ class pdfImposing:
         print("done")
 
     def booklet(self):
-        scripts.booklet(self.FILES)
+        scripts.booklet(self.FILES, float(self.margin.get()))
         print("done")
 
     def SimplexStackCut(self):
